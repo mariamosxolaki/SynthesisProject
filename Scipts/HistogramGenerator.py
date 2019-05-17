c5e93a2e8dec111e8c6ce5dc0cf3352c49fef6d9
MrConneDome <konradjarocki@gmail.com>
2019-05-17 20:16:39 +0200

    Update HistogramGenerator.py

--- Scipts/HistogramGenerator.py
+++ Scipts/HistogramGenerator.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 15 19:28:32 2019
 
+"""
+main part of the script for histogram generator. Saving the file to txt, using threshold.py and get_bb.py to automate the process.
 @author: Konrad Jarocki
 """
 
@@ -9,45 +8,72 @@ Created on Wed May 15 19:28:32 2019
 import psycopg2
 import numpy as np
 import matplotlib.pyplot as plt
+import get_bb
+import threshold
+
 #Connect to postgresql database with postGIS extension installed
 conn = psycopg2.connect("dbname=histogram user = postgres")
 cur = conn.cursor()
-#table_name = 'point_cloud1'
-#cur.execute("select min(x) as x_min, min(y) as y_min, max(x) as x_max, max(y) as y_max from {};".format(table_name)) 
-#Bounding box of the point cloud, required to estimate the size of the grid
-x_min = -61874.023437500
-y_min = -26957.539062500
-x_max = -60830.167968750
-y_max = -26137.185546880
 
-#Define the grid
-grid_width = abs(x_max-x_min)
-grid_height = abs(y_max-y_min)
-cell_size = 1
-cell_width = int(round(grid_width / cell_size,0) +1)
-cell_height = int(round(grid_height / cell_size,0) +1)
-print(cell_width,cell_height)
-histogram = [[0 for x in range(cell_width)] for y in range(cell_height)] 
-#Iterating every cell in the grid
-for i in range(cell_width): 
-    for j in range(cell_height): 
-        #making boundaries for specific cell
-        gx_min = x_min + cell_size*i
-        gy_min = y_min + cell_size*j
-        gx_max = x_min + cell_size*(i+1)
-        gy_max = y_min + cell_size*(j+1)
-        #query
-        cur.execute("SELECT count(*) FROM point_cloud1 where x>={} and x<{} and y>={} and y<{};".format(gx_min,gx_max, gy_min, gy_max)) 
-        for record in cur:
-            print(i,j,record[0])
-            #save the result to array
-            histogram[j][i] = record[0]
+#predefined size of the cell in [m]
+cell_size = 300
+
+#defining all the parameters of the grid
+def define_grid(bb_box, cell_size):
+    grid_par = [0 for x in range(4)]
+    x_min = bb_box[0]
+    x_max = bb_box[1]
+    y_min = bb_box[2]
+    y_max = bb_box[3]
+    grid_width = abs(x_max-x_min)
+    grid_height = abs(y_max-y_min)
+    
+    #number of cells in rows and columns
+    cell_width = int(round(grid_width / cell_size,0) +1)
+    cell_height = int(round(grid_height / cell_size,0) +1)
+    
+    print('number of cells width: ',cell_width, ' number of cells height: ',cell_height)
+    
+    grid_par[0] = grid_width
+    grid_par[1] = grid_height
+    grid_par[2] = cell_width
+    grid_par[3] = cell_height
+    
+    return grid_par
+
+    
+def generate_histogram(grid_par, bb_box, cell_size):
+    
+    histogram = [[0 for x in range(grid_par[2])] for y in range(grid_par[3])] 
+    
+    #Iterating every cell in the grid
+    for i in range(grid_par[2]): 
+        for j in range(grid_par[3]): 
             
+            #making boundaries for specific cell
+            gx_min = x_min + cell_size*i
+            gy_min = y_min + cell_size*j
+            gx_max = x_min + cell_size*(i+1)
+            gy_max = y_min + cell_size*(j+1)
+            
+            #query
+            cur.execute("SELECT count(*) FROM point_cloud1 where x>={} and x<{} and y>={} and y<{};".format(gx_min,gx_max, gy_min, gy_max)) 
+            
+            for record in cur:
+                print(i,j,record[0])
+                
+                #save the result to array
+                histogram[j][i] = record[0]
+    return histogram
+
+
+bb_box = get_bb.getbb('histogram','postgres','point_cloud1')
+grid_par = define_grid(bb_box, cell_size)
+histogram = generate_histogram(grid_par,bb_box,cell_size)
+
+#plot of the results and save to file
 f = np.array(histogram)  
 np.savetxt('histogram_{}m.txt'.format(cell_size),f)           
-#plot of the results
-print(histogram)
-      
 plt.imshow(f, interpolation="nearest", origin="upper")
 plt.colorbar()
 plt.show()
